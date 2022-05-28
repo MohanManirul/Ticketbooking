8e0ef75343f2001c7610ccc473b000ade2330043
MohanManirul <fiforeg@gmail.com>
2022-05-28 15:54:47 +0600

    global and local component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
-    <div>
-        <h2>{{ msg }}</h2>
-        <!-- uses contact-details components from ContactDetails.vue page-->
-        
-        <contact-details></contact-details> 
-        <contact-details></contact-details>
-        <contact-details></contact-details>
-        
-        <!-- uses contact-details components from ContactDetails.vue page-->
-    </div>
+  <div>
+    <h2>{{ msg }}</h2>
+    <!-- uses contact-details components from ContactDetails.vue page-->
+
+    <contact-details></contact-details>
+    <contact-details></contact-details>
+    <contact-details></contact-details>
+
+    <!-- uses contact-details components from ContactDetails.vue page-->
+  </div>
 </template>
 
 <script>
-   export default {
-       data() {
-        return {
-            msg: "welcome to Vue 3 Tutorial fgfg"
-        };
-    },
-    
-   }
+import ContactDetails from "./ContactDetails.vue";
+
+export default {
+  data() {
+    return {
+      msg: "welcome to Vue 3 Tutorial fgfg",
+    };
+  },
+  components: { ContactDetails },
+};
 </script>
